4a904a05a373dc2aa3d3cfabb2eb45223b584b38
Evgen Savinov <evs@sitecore.net>
2016-09-27 17:34:45 +0300

    Patch fixes the issue with "Lock and edit" button when an item in the final workflow state. Initial commit

---
+++ src/Sitecore.Support.126524/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.126524")]
+[assembly: AssemblyProduct("Sitecore.Support.126524")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
