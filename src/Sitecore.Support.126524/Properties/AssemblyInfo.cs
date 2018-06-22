382953ada5cfe0d7133b988ae6794853b586bdc0
SC-IDO <ido@sitecore.net>
2018-06-22 12:31:23 +0300

    Original functionality

---
+++ src/Sitecore.Support.126524/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.126524")]
+[assembly: AssemblyProduct("Sitecore.Support.126524")]
+[assembly: ComVisible(false)]
